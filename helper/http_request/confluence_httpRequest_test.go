a15f9461bc070c41a28427a885ee8921156f1b7c
mr nim <mrnim@nimtechnology.com>
2025-06-22 09:24:12 +0700

    the unit test

---
+++ helper/http_request/confluence_httpRequest_test.go
@@ -0,0 +1,40 @@
+package http_request
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCreateConfluencePage(t *testing.T) {
+	os.Setenv("EMAIL", "test@example.com")
+	os.Setenv("API_TOKEN", "testtoken")
+	os.Setenv("CONFLUENCE_URL", "http://localhost")
+	defer os.Unsetenv("EMAIL")
+	defer os.Unsetenv("API_TOKEN")
+	defer os.Unsetenv("CONFLUENCE_URL")
+
+	resp, err := CreateConfluencePage("SPACE", "PARENT", "TestTitle", "TestBody")
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if resp == nil {
+		t.Error("expected response, got nil")
+	}
+}
+
+func TestGetConfluencePagesByTitle(t *testing.T) {
+	os.Setenv("EMAIL", "test@example.com")
+	os.Setenv("API_TOKEN", "testtoken")
+	os.Setenv("CONFLUENCE_URL", "http://localhost")
+	defer os.Unsetenv("EMAIL")
+	defer os.Unsetenv("API_TOKEN")
+	defer os.Unsetenv("CONFLUENCE_URL")
+
+	resp, err := GetConfluencePagesByTitle("TestTitle")
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if resp == nil {
+		t.Error("expected response, got nil")
+	}
+}
