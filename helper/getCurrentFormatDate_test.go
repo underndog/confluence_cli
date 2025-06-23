7d10ed8b2e5e0aab27cba8e918735007f544d7eb
mr nim <mrnim@nimtechnology.com>
2025-06-23 10:27:56 +0700

    update typo

--- helper/getCurrentFormatDate_test.go
+++ helper/getCurrentFormatDate_test.go
@@ -5,16 +5,16 @@ import (
 	"time"
 )
 
-func TestgetCurrentFormattedDate(t *testing.T) {
+func TestGetCurrentFormattedDate(t *testing.T) {
 	format := "2006-01-02"
 	date := getCurrentFormattedDate(format)
 	// Validate the date format using time.Parse
 	if _, err := time.Parse(format, date); err != nil {
 		t.Errorf("date '%s' does not match expected format '%s': %v", date, format, err)
 	}
-	
+
 	// Additional check for expected length
 	if len(date) != len(format) {
 		t.Errorf("expected date format length %d, got %d", len(format), len(date))
- 	}
+	}
 }
